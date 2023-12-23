48100d09610e3571db02cfa730bb49fb52f1e837
zahid-soft <zahidsarang95@gmail.com>
2023-12-23 20:10:46 +0530

    setup media stream

---
+++ hooks/useMediaStream.js
@@ -0,0 +1,27 @@
+const { useState, useEffect, useRef } = require("react");
+
+const useMediaStream = () => {
+	const [state, setState] = useState(null);
+	const isStreamSet = useRef(false);
+
+	useEffect(() => {
+		if (isStreamSet.current) return;
+		isStreamSet.current = true;
+		(async function initStream() {
+			try {
+				const stream = await navigator.mediaDevices.getUserMedia({
+					audio: true,
+					video: true,
+				});
+				console.log("setting your strea");
+				setState(stream);
+			} catch (e) {
+				console.log("Error in media navigator", e);
+			}
+		})();
+	}, []);
+	return {
+		stream: state,
+	};
+};
+export default useMediaStream;
